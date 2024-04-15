282d9cfefad8f49a26d23ae7a6bfaf03dc017615
Elena <ewlen04@gmail.com>
2024-04-15 18:23:40 +0200

    configure env for connection string and API key

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://ewelty:Kl5m9Gt624Fz@cluster0.c7a5tyj.mongodb.net/weatherapp-part4';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
